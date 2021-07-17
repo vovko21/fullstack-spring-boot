359c5c8bc5ddfb726e32857137ef0eebfd5e9ab6
novakvova <novakvova@gmail.com>
2021-07-17 17:54:13 +0300

    Add proxi and CorsConfig

---
+++ src/main/java/com/example/springboot/configure/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.example.springboot.configure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false); //updated to false
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
--- src/main/java/com/example/springboot/controllers/api/AnimalController.java
+++ src/main/java/com/example/springboot/controllers/api/AnimalController.java
@@ -3,9 +3,7 @@ package com.example.springboot.controllers.api;
 import com.example.springboot.entities.Animal;
 import com.example.springboot.repositories.AnimalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
