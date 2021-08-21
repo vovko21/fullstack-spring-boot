82745d3c61ef519d69bc9188540b49422eae5b9b
novakvova <novakvova@gmail.com>
2021-08-21 17:25:32 +0300

    Add auth get roles

--- src/main/java/com/example/springboot/configure/security/JwtTokenUtil.java
+++ src/main/java/com/example/springboot/configure/security/JwtTokenUtil.java
@@ -1,6 +1,8 @@
 package com.example.springboot.configure.security;
 
 import com.example.springboot.entities.User;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
@@ -9,9 +11,12 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.SignatureException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Type;
 import java.util.Date;
+import java.util.List;
 
 import static java.lang.String.format;
 
@@ -21,10 +26,23 @@ public class JwtTokenUtil {
 
     private final String jwtSecret = "zdtlD3JK56m6wTTgsNFhqzjqP";
     private final String jwtIssuer = "example.io";
+    @Autowired
+    private Gson gson;
 
     public String generateAccessToken(User user) {
+        String [] userRoles = user.getRoles().stream()
+                .map((role) -> role.getName()).toArray(String []:: new);
+
+//        Type type = new TypeToken<List<String[]>>() {}.getType();
+
+        String roles = gson.toJson(userRoles);
+
+        //System.out.println(roles);
+
         return Jwts.builder()
                 .setSubject(format("%s,%s", user.getId(), user.getUsername()))
+                .claim("username", user.getUsername())
+                .claim("roles", roles)
                 .setIssuer(jwtIssuer)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 7 * 24 * 60 * 60 * 1000)) // 1 week
