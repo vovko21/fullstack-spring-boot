0125aeb9a1064c95e2f19a86f8e0fa93fee45b63
novakvova <novakvova@gmail.com>
2021-08-21 16:20:25 +0300

    Add auth login

--- src/main/java/com/example/springboot/configure/security/SecurityConfig.java
+++ src/main/java/com/example/springboot/configure/security/SecurityConfig.java
@@ -83,7 +83,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // Set permissions on endpoints
         http.authorizeRequests()
                 // Swagger endpoints must be publicly accessible
-                //.antMatchers("/").hasAuthority(Roles.Admin)
+                .antMatchers("/api/animals").hasAuthority(Roles.Admin)
                 .antMatchers(String.format("%s/**", restApiDocPath)).permitAll()
                 .antMatchers(String.format("%s/**", swaggerPath)).permitAll()
                 .antMatchers("/api/public/**").permitAll()
